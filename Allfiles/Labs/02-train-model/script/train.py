5c937c6f3b2603b0e35cec6febafa001ef28df75
Tomás Osório <tomas.s.osorio@gmail.com>
2024-02-15 10:43:06 +0000

    Allow train model when arg no-cuda is True

--- Allfiles/Labs/02-train-model/script/train.py
+++ Allfiles/Labs/02-train-model/script/train.py
@@ -159,7 +159,7 @@ def main():
 
     torch.manual_seed(args.seed)
 
-    device = torch.device("cuda")
+    device = torch.device("cuda" if use_cuda else "cpu")
 
     kwargs = {"batch_size": args.batch_size}
     if use_cuda:
